33196a81e48c6ab002275f42e974485aed3adf83
Ugur Ayan <ugur.ayan@gmail.com>
2017-09-18 17:15:28 +0200

    update Random and All Spinners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  show = false;
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {SpinnerComponent} from '../app/spinner/spinner.component'
+import {SpinnerComponent} from '../app/spinner/spinner.component';
+import {AllComponent} from '../app/all/all.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    AllComponent
   ],
   imports: [
     BrowserModule
